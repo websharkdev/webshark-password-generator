8a7778982cc664343a6619b86a2d58b53e52647f
alex.bortnitskyi <alexey@neonpilots.co.uk>
2022-10-29 21:34:04 +0200

    bugfix «pages\index.tsx» v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,12 @@ import { Home } from '@/screens/Home'
 
 import Meta from '@/utils/meta'
 
-const HomePage: FC = () => {
-  return (
-    <Meta title="Home" description="Some description of page.">
-      <Layout>
-        <Home />
-      </Layout>
-    </Meta>
-  )
-}
+const HomePage: FC = () => (
+  <Meta title="Home" description="Some description of page.">
+    <Layout>
+      <Home />
+    </Layout>
+  </Meta>
+)
 
 export default HomePage
