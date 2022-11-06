a0f08d143aa528dc8924ca94763608a7c74b5e06
alex.bortnitskyi <alexey@neonpilots.co.uk>
2022-11-06 20:17:49 +0100

    have been add new block - footer

--- app/assets/icons/ui/index.ts
+++ app/assets/icons/ui/index.ts
@@ -1,8 +1,24 @@
 import BurgerClosetIcon from './burger_closed.svg'
 import BurgerOpenedIcon from './burger_opened.svg'
+import buyMeACoffeQR from './buyMeACoffeQR.svg'
 import CloseIcon from './close.svg'
 import CopyIcon from './copy.svg'
 import EthereumIcon from './ethereum.svg'
+// SOCIAL
+import GitHubIcon from './github.svg'
+import InstagramIcon from './instagram.svg'
+import TelegramIcon from './telegram.svg'
 import TrashIcon from './trash.svg'
 
-export { EthereumIcon, TrashIcon, CopyIcon, CloseIcon, BurgerClosetIcon, BurgerOpenedIcon }
+export {
+  EthereumIcon,
+  TrashIcon,
+  CopyIcon,
+  CloseIcon,
+  BurgerClosetIcon,
+  BurgerOpenedIcon,
+  buyMeACoffeQR,
+  GitHubIcon,
+  InstagramIcon,
+  TelegramIcon,
+}
